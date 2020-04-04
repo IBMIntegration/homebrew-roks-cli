dd3511dbb5b3297d05ad644cc95ed4ea194a7fbd
Jamil Spain <jamil.spain@us.ibm.com>
2020-04-03 23:33:11 -0400

    updating to v0.3.3.. fixing the binary renaming

--- roks-cli.rb
+++ roks-cli.rb
@@ -1,24 +1,24 @@
 class RoksCli < Formula
     desc "Toolkit to work with Cloud Pak for Integration & OpenShift with golang cli"
     homepage "https://github.com/ibmintegration/roks-cli"
-    url "https://github.com/ibmintegration/roks-cli/releases/download/v0.3.1/roks-cli_darwin_amd64"
-    version "0.3.1"
+    url "https://github.com/ibmintegration/roks-cli/releases/download/v0.3.3/roks-cli_0.3.3_macos_x86_64"
+    version "0.3.3"
     sha256 "33ba534fe16ae4084f25326954e296dd2d7fabd110d7b68c1be0a849a365b140"
   
-    option "with-goswagger", "Names the binary goswagger instead of swagger"
+    option "with-rokscli", "Names the binary rokscli instead of roks"
   
     def install
       nm = "roks"
-      if build.with? "goswagger"
-        nm = "goswagger"
+      if build.with? "rokscli"
+        nm = "roks-cli"
       end
-      system "mv", "roks-cli_darwin_amd64", nm
+      system "mv", "roks-cli_0.3.3_macos_x86_64", nm
       bin.install nm
     end
   
     test do
-      if build.with? "goswagger"
-        system "#{bin}/roks", "version"
+      if build.with? "rokscli"
+        system "#{bin}/roks-cli", "version"
       else
         system "#{bin}/roks", "version"
       end
